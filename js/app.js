0aed8d96be4558758e19e5f0acb3dad0c573ca9d
guillaume <guillaumechaulet@hotmail.fr>
2020-05-09 15:13:46 +0200

    9.5 fourth  style true-value

--- js/app.js
+++ js/app.js
@@ -3,23 +3,15 @@ new Vue ({
     data: {
        message : 'Salut les gens',
        link:'https://google.com',
-       success: true,
+       cls: 'success',
        persons: ['Jonathan', 'Marion', 'Jean', 'Patrick'],
        style: { background: '#0000'}
     },
     methods: {
         close: function(){
-            this.message = "fermé"
+
             this.success = false
         },
-        style: function(){
-            if(this.success) {
-               return {background: '#00FF00'} 
-            } else {
-                return { 
-                    background: '#FF0000'
-                }
-            } 
-        }
+       
     }
 })
