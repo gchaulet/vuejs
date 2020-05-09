6e059ac9e05f4f0bfda931ef4fcfd137429c96f2
guillaume <guillaumechaulet@hotmail.fr>
2020-05-09 14:58:41 +0200

    9.5 second ternaire

--- js/app.js
+++ js/app.js
@@ -3,7 +3,23 @@ new Vue ({
     data: {
        message : 'Salut les gens',
        link:'https://google.com',
-       success: false,
-       persons: ['Jonathan', 'Marion', 'Jean', 'Patrick']
+       success: true,
+       persons: ['Jonathan', 'Marion', 'Jean', 'Patrick'],
+       style: { background: '#0000'}
+    },
+    methods: {
+        close: function(){
+            this.message = "fermé"
+            this.success = false
+        },
+        style: function(){
+            if(this.success) {
+               return {background: '#00FF00'} 
+            } else {
+                return { 
+                    background: '#FF0000'
+                }
+            } 
+        }
     }
 })
