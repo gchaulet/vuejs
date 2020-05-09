14f6c8e70b472bc9e4210704273ab90423c32060
guillaume <guillaumechaulet@hotmail.fr>
2020-05-09 17:37:37 +0200

    9.5 thirtheen own directive

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 Vue.directive('salut', {
     bind: function (el, binding, vnode){
-        console.log(el, binding)
+        el.value = binding.value
+        console.log('bind')
+    },
+    update: function (el, binding, vnode, oldvnode){
+        console.log('update')
     }
 })
 let vm = new Vue ({
