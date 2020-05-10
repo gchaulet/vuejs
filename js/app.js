1e5dd6b5d8dc56870cac485b535a84b575851190
guillaume <guillaumechaulet@hotmail.fr>
2020-05-10 09:25:53 +0200

    10.5 sixtheenth component part 2

--- js/app.js
+++ js/app.js
@@ -6,9 +6,30 @@ let message = {
     template: `<div class="ui message " :class="type">{{ message }}</div>`
 }
 
+let counter = {
+    //function in data property due to the 2 counter must be independent to give 2 different state
+    data: function() {
+        return {
+            count: 0
+        }
+    },
+    props : {
+        start: {type: Number, default: 0}
+    },
+    methods: {
+        increment: function() {
+            this.count++
+        }
+    },
+    template: `<button @click="increment">{{ count }}</button>`,
+    mounted: function(){
+        this.count = this.start
+    }
+}
+
 let vm = new Vue ({
     el: '#app',
-    components: { message },
+    components: { message, counter },
     data: {
        message: 'Le meilleur best',
     },
